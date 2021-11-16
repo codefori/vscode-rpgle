271cea205c349b9d8291fe921f6c6eaa0f34d982
Liam Barry Allan <liam@worksofbarry.com>
2021-11-16 15:28:37 -0500

    Init commit

---
+++ extension.js
@@ -0,0 +1,32 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require(`vscode`);
+
+const Worker = require(`./src/worker`);
+
+const { instance } = vscode.extensions.getExtension(`halcyontechltd.code-for-ibmi`).exports;
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(`Congratulations, your extension "vscode-rpgle" is now active!`);
+
+  instance.on(`connected`, () => {
+    new Worker(context);
+  })
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+}
