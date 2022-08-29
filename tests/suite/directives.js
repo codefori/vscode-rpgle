f5a75c2b72ea7c8c2a316cd4d61c4ba4c395132f
Liam Barry Allan <mrliamallan@live.co.uk>
2022-08-29 10:33:46 -0400

    Fixed test for if directive
    Signed-off-by: Liam Barry Allan <mrliamallan@live.co.uk>

--- tests/suite/directives.js
+++ tests/suite/directives.js
@@ -254,5 +254,43 @@ module.exports = {
     assert.strictEqual(Prp00a.subItems.length, 7);
     assert.strictEqual(Prp00a.keyword[`QUALIFIED`], true);
     assert.strictEqual(Prp00a.keyword[`TEMPLATE`], undefined);
+  },
+
+  if2: async () => {
+    const lines = [
+      `     D ObjNam          s             10a`,
+      `     d someDs          ds`,
+      `       /IF DEFINED(RPGBNV)`,
+      `     d                                     based(somepointer)`,
+      `       /ENDIF`,
+      `     d  xxxxxx                       10i 0`,
+      `     d  xxxxxxxx                     10i 0`,
+      `     d  xxxxxx                       20i 0`,
+      `     d  xxx                          10i 0`,
+      `     d  xxxxx                        10i 0`,
+      `     d  yyyyy                        10i 0`,
+      `     d  zzzzz                        10i 0`,
+      `     d  fffffff                        N`,
+      `     d  jjjjj                          N`,
+      `     d  jjjjjjj                        N`,
+      `     d  mmmmm                        10`,
+      `     d  cccccc                        3`,
+      `     d  bbbbbbd                      10i 0`,
+      `     d  dddd                         10i 0`,
+      `     d  ddddd                        10i 0`,
+      `     d  bbbbb                        10i 0`,
+      `     d  ccc                          10i 0`,
+      `     d  bbbwee                        7`,
+      `     d  fffbb                        10i 0`,
+      `     d  ff                         1024a`,
+    ].join(`\n`);
+
+    const parser = new Parser();
+    const cache = await parser.getDocs(uri, lines);
+
+    assert.strictEqual(cache.structs.length, 1);
+    
+    const someDs = cache.find(`someDs`);
+    assert.strictEqual(someDs.keyword[`BASED`], undefined);
   }
 }
