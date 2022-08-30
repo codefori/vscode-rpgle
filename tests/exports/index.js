9e85f9904a02bc6f482343619030b363b00ca276
Liam Barry Allan <mrliamallan@live.co.uk>
2022-08-30 10:50:05 -0400

    Support structs in API generator
    Signed-off-by: Liam Barry Allan <mrliamallan@live.co.uk>

--- tests/exports/index.js
+++ tests/exports/index.js
@@ -27,10 +27,8 @@ Module.prototype.require = function(){
 const fs = require(`fs/promises`);
 
 const Parser = require(`../../src/language/parser`);
-const Linter = require(`../../src/language/linter`);
-const path = require(`path`);
 
-const uri = vscode.Uri.parse(`source.rpgle`);
+const uri = vscode.Uri.parse(`protos.rpgle`);
 
 const doWork = async () => {
   const content = await  fs.readFile(`./protos.rpgle`, {encoding: `utf-8`});
@@ -40,6 +38,7 @@ const doWork = async () => {
 
   /** @type {{[name: string]: APIInterface}} */
   const results = {};
+
   cache.procedures.forEach(proc => {
     let detail = Object.keys(proc.keyword).filter(k => !k.startsWith(`EXT`)).map(k => {
       if (proc.keyword[k] === true) {
@@ -63,16 +62,44 @@ const doWork = async () => {
       prototype: [
         `///`,
         `// ${proc.name}`,
-        `// ${proc.description}`,
+        `// ${proc.description || `API`}`,
+        ...proc.tags.map(tag => `// @${tag.tag} ${tag.content}`),
         `///`,
-        `Dcl-Pr ${proc.name} ${proc.keywords.join(` `)};`,
+        `dcl-pr ${proc.name} ${proc.keywords.join(` `)};`,
         ...proc.subItems.map(subItem => 
           `  ${subItem.name} ${subItem.keywords.join(` `)};`
         ),
-        `End-Pr;`
+        `end-pr;`
       ]
     }
   });
+
+  cache.structs
+    .filter(s => s.keyword[`TEMPLATE`] === true)
+    .forEach(struct => {
+      let parms = ``;
+      if (struct.subItems.length > 0)
+        parms = `\n${struct.subItems.map((p, i) => `  \${${i+1}:${p.name}}`).join(`:\n`)}\n`;
+    
+      results[struct.name] = {
+        type: `struct`,
+        description: struct.description,
+        detail: struct.keywords.join(` `),
+        insertText: struct.name,
+        prototype: [
+          `///`,
+          `// ${struct.name}`,
+          `// ${struct.description || `Struct`}`,
+          ...struct.tags.map(tag => `// @${tag.tag} ${tag.content}`),
+          `///`,
+          `dcl-ds ${struct.name} ${struct.keywords.join(` `)};`,
+          ...struct.subItems.map(subItem => 
+            `  ${subItem.name} ${subItem.keywords.join(` `)};`
+          ),
+          `end-ds;`
+        ]
+      }
+    });
 
   await fs.writeFile(`./output.json`, JSON.stringify(results, null, 2), {
     encoding: `utf-8`
