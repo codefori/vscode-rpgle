c0bc3ff3b291a3d2fa47b099c01f50039159a444
Liam Barry Allan <mrliamallan@live.co.uk>
2022-10-29 14:48:45 -0400

    EXPORT references
    Signed-off-by: Liam Barry Allan <mrliamallan@live.co.uk>

--- server/src/providers/project/index.ts
+++ server/src/providers/project/index.ts
@@ -22,7 +22,7 @@ export async function initialise() {
 				case FileChangeType.Changed:
 					loadFile(fileEvent.uri);
 					break;
-					
+
 				default:
 					parser.clearParsedCache(fileEvent.uri);
 					break;
@@ -62,41 +62,113 @@ async function loadFile(uri: string) {
 export function findAllReferences(def: Declaration): Location[] {
 	let locations: Location[] = [];
 
+	const parsedFiles = Object.keys(parser.parsedCache);
+
+	const document = documents.get(def.position.path);
+
+	if (document) {
+		locations.push(
+			...def.references.map(ref => Location.create(
+				def.position.path,
+				calculateOffset(document, ref)
+			))
+		);
+	}
+
 	if (isEnabled) {
-		const baseUri = def.position.path;
+		if (def.keyword[`EXPORT`]) {
+			// If we are looking for references to an export function
+			// scan entire project for `EXTPROC` definitions that point to this
+			const upperName = def.name.toUpperCase();
 
-		const parsedCache = Object.keys(parser.parsedCache);
+			for (const keyPath of parsedFiles) {
+				const document = documents.get(keyPath);
 
-		parsedCache.forEach(uri => {
-			const document = documents.get(uri);
-			if (document) {
-				const cache = parser.getParsedCache(uri);
+				if (document) {
+					const cache = parser.getParsedCache(keyPath);
 
-				// It's only a valid reference if it is imported somewhere else
-				const foundInclude = cache.includes.find(include => include.toPath === baseUri)
-				if (foundInclude) {
-					const possibleDef = cache.find(def.name);
+					cache.procedures.forEach(proc => {
+						let addReference = false;
+						const keyword = proc.keyword[`EXTPROC`];
+						if (keyword) {
+							if (keyword === true) {
+								if (proc.name.toUpperCase() === upperName) {
 
-					// Okay, we found something with a similar name in another file...
-					if (possibleDef) {
-						if (possibleDef.position.path === def.position.path) {
-							locations.push(
-								// First, we push the copybook where it is brought in.
-								// We do this because we don't have references for non-**free
-								Location.create(uri, Range.create(foundInclude.line, 0, foundInclude.line, 0)),
+									addReference = true;
+								}
+							} else
+								if (trimQuotes(keyword).toUpperCase() === upperName) {
+									addReference = true;
+								}
+						} else
 
-								// Then we push the references. Empty for non-**free
-								...possibleDef.references.map(ref => Location.create(
-									uri,
-									calculateOffset(document, ref)
-								))
-							);
+							// Also turns out, any `DCL-PR` without any keywords is `EXTPROC` by default.
+							if (!proc.keyword[`EXPORT`] && proc.name.toUpperCase() === upperName) {
+								addReference = true;
+							}
+
+						if (addReference) {
+							// Don't add duplicates
+							if (!locations.some(loc => loc.uri === keyPath)) {
+								locations.push(
+									// First, we push the copybook where it is brought in.
+									// We do this because we don't have references for non-**free
+									Location.create(proc.position.path, Range.create(proc.position.line, 0, proc.position.line, 0)),
+
+									// Then we push the references. Empty for non-**free
+									...proc.references.map(ref => Location.create(
+										keyPath,
+										calculateOffset(document, ref)
+									))
+								);
+							}
+						}
+
+					})
+				}
+			}
+
+		} else {
+			// Otherwise, we are looking for references to the current definition
+			const baseUri = def.position.path;
+
+			parsedFiles.forEach(uri => {
+				const document = documents.get(uri);
+				if (document) {
+					const cache = parser.getParsedCache(uri);
+
+					// It's only a valid reference if it is imported somewhere else
+					const foundInclude = cache.includes.find(include => include.toPath === baseUri)
+					if (foundInclude) {
+						const possibleDef = cache.find(def.name);
+
+						// Okay, we found something with a similar name in another file...
+						if (possibleDef) {
+							if (possibleDef.position.path === def.position.path) {
+								locations.push(
+									// First, we push the copybook where it is brought in.
+									// We do this because we don't have references for non-**free
+									Location.create(uri, Range.create(foundInclude.line, 0, foundInclude.line, 0)),
+
+									// Then we push the references. Empty for non-**free
+									...possibleDef.references.map(ref => Location.create(
+										uri,
+										calculateOffset(document, ref)
+									))
+								);
+							}
 						}
 					}
 				}
-			}
-		})
+			})
+		}
 	}
 
 	return locations;
+}
+
+function trimQuotes(input: string) {
+	if (input[0] === `'`) input = input.substring(1);
+	if (input[input.length - 1] === `'`) input = input.substring(0, input.length - 1);
+	return input;
 }
