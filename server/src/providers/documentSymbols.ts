7be59e7e91db0ced549de587becea6aede5fd58c
Liam Barry Allan <mrliamallan@live.co.uk>
2023-01-27 15:37:01 -0500

    Fix for bad document symbol pushing
    Signed-off-by: Liam Barry Allan <mrliamallan@live.co.uk>

--- server/src/providers/documentSymbols.ts
+++ server/src/providers/documentSymbols.ts
@@ -42,7 +42,7 @@ export default async function documentSymbolProvider(handler: DocumentSymbolPara
 						procDef.children.push(...getScopeVars(proc.scope));
 					}
 
-					symbols.push(procDef);
+					currentScopeDefs.push(procDef);
 				});
 
 			currentScopeDefs.push(
