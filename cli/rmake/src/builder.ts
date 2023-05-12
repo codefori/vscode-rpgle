0ec7013ee7d416f2cb57af02a3ab4bcc67ace2e3
Liam Allan <mrliamallan@live.co.uk>
2023-05-12 13:53:31 -0500

    Base of builder done
    Signed-off-by: Liam Allan <mrliamallan@live.co.uk>

--- cli/rmake/src/builder.ts
+++ cli/rmake/src/builder.ts
@@ -2,7 +2,7 @@ import glob from 'glob';
 import path from 'path';
 import Cache from '../../../server/src/language/models/cache';
 
-type ObjectType = "PGM"|"SRVPGM"|"MODULE"|"FILE";
+type ObjectType = "PGM" | "SRVPGM" | "MODULE" | "FILE";
 
 interface ILEObject {
 	name: string;
@@ -11,17 +11,17 @@ interface ILEObject {
 }
 
 interface ILEObjectTarget extends ILEObject {
-	deps: ILEObjectTarget[];
+	deps: ILEObject[];
 }
 
 export class Builder {
-	private pathCache: {[path: string]: true}|undefined;
-	private resolvedPaths: {[query: string]: string} = {};
-	private resolvedObjects: {[localPath: string]: ILEObject} = {};
+	private pathCache: { [path: string]: true } | undefined;
+	private resolvedPaths: { [query: string]: string } = {};
+	private resolvedObjects: { [localPath: string]: ILEObject } = {};
 
 	private targets: ILEObjectTarget[];
 
-	constructor(private cwd: string) {}
+	constructor(private cwd: string) { }
 
 	public resolveObject(localPath: string) {
 		if (this.resolvedObjects[localPath]) return this.resolvedObjects[localPath];
@@ -98,13 +98,73 @@ export class Builder {
 	}
 
 	public createTarget(localPath: string, cache: Cache) {
+		const sourceName = path.basename(localPath);
 		const ileObject = this.resolveObject(localPath);
 		const target: ILEObjectTarget = {
 			...ileObject,
 			deps: []
 		};
 
-		
+		// Find external programs
+		cache.procedures
+			.filter((proc: any) => proc.keyword[`EXTPGM`])
+			.map(ref => {
+				const keyword = ref.keyword;
+				let fileName = ref.name;
+				const extpgm = keyword[`EXTPGM`];
+				if (extpgm) {
+					if (extpgm === true) fileName = ref.name;
+					else fileName = trimQuotes(extpgm);
+				}
+
+				return fileName + `.pgm`;
+			})
+			.forEach(ref => {
+				const resolvedPath = this.resolveLocalObjectQuery(ref, sourceName);
+				if (resolvedPath) target.deps.push(this.resolveObject(resolvedPath));
+			});
+
+		// Find external data structure sources
+		cache.structs
+			.filter((struct: any) => struct.keyword[`EXTNAME`])
+			.map(struct => {
+				const keyword = struct.keyword;
+				return trimQuotes(keyword[`EXTNAME`]).toLowerCase();
+			})
+			.forEach(ref => {
+				const resolvedPath = this.resolveLocalObjectQuery(ref, sourceName);
+				if (resolvedPath) target.deps.push(this.resolveObject(resolvedPath));
+			});
+
+		cache.files
+			.map(file => file.name)
+			.forEach(ref => {
+				const resolvedPath = this.resolveLocalObjectQuery(ref, sourceName);
+				if (resolvedPath) target.deps.push(this.resolveObject(resolvedPath));
+			})
+
+		// We ignore anything with hardcoded schemas
+		cache.sqlReferences
+			.filter(ref => !ref.description)
+			.map(ref => ref.name)
+			.forEach(ref => {
+				const resolvedPath = this.resolveLocalObjectQuery(ref, sourceName);
+				if (resolvedPath) target.deps.push(this.resolveObject(resolvedPath));
+			});
+
+		this.targets.push(target);
+	}
+
+	public generateMake(): string[] {
+		let lines = [];
+
+		for (const target of this.targets) {
+			lines.push(
+				`$(PREPATH)/${target.name}.${target.type}: ${target.deps.map(dep => `$(PREPATH)/${dep.name}.${dep.type}`).join(` `)}`
+			)
+		};
+
+		return lines;
 	}
 }
 
