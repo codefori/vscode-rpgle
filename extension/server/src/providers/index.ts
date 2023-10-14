9aef76546d6bdbe84a51afe136e1bb21b26b7540
worksofliam <mrliamallan@live.co.uk>
2023-10-14 12:33:06 -0400

    Ability to prepare on the variable

--- extension/server/src/providers/index.ts
+++ extension/server/src/providers/index.ts
@@ -41,3 +41,30 @@ export function getWordRangeAtPosition(document: TextDocument, position: Positio
 	else
 		return document.getText(Range.create(line, Math.max(0, startChar), line, endChar+1)).replace(/(\r\n|\n|\r)/gm, "");
 }
+
+export function getRangeAtPosition(document: TextDocument, position: Position): {value: string, range: Range}|undefined {
+	const lines = document.getText().split(`\n`); // Safe to assume \n because \r is then at end of lines
+	const line = Math.min(lines.length - 1, Math.max(0, position.line));
+	const lineText = lines[line];
+	const character = Math.min(lineText.length - 1, Math.max(0, position.character));
+
+	let startChar = character;
+	while (startChar > 0 && !/[\s\W]/.test(lineText.charAt(startChar - 1))) {
+		startChar -= 1;
+	}
+
+	let endChar = character;
+	while (endChar < lineText.length && (!/[\s\W]/.test(lineText.charAt(endChar + 1)))) {
+		endChar += 1;
+	}
+
+	if (startChar === endChar)
+		return undefined;
+	else {
+		const theRange = Range.create(line, Math.max(0, startChar), line, endChar+1);
+			return {
+				value: document.getText(theRange).replace(/(\r\n|\n|\r)/gm, ""),
+				range: theRange
+			};
+		}
+}
--- extension/server/src/providers/rename.ts
+++ extension/server/src/providers/rename.ts
@@ -1,51 +1,56 @@
 
-import { documents, getWordRangeAtPosition, parser } from '.';
+import { documents, getRangeAtPosition, getWordRangeAtPosition, parser } from '.';
 import { PrepareRenameParams, Range, RenameParams, WorkspaceEdit } from "vscode-languageserver";
 import Linter from '../../../../language/linter';
 
-export async function renamePrepareProvider(params: PrepareRenameParams): Promise<Range|undefined> {
+export async function renamePrepareProvider(params: PrepareRenameParams): Promise<Range | undefined> {
   const uri = params.textDocument.uri;
-	const currentPos = params.position;
-	const document = documents.get(uri);
+  const currentPos = params.position;
+  const document = documents.get(uri);
 
-	if (document) {
-		const isFree = (document.getText(Range.create(0, 0, 0, 6)).toUpperCase() === `**FREE`);
+  if (document) {
+    const isFree = (document.getText(Range.create(0, 0, 0, 6)).toUpperCase() === `**FREE`);
 
-		let word = getWordRangeAtPosition(document, currentPos)?.trim();
+    let selected = getRangeAtPosition(document, currentPos);
+    if (selected) {
+      let word = selected.value.trim();
 
-		if (word) {
-      if (word.endsWith(`;`)) {
-        const pieces = word.split(`;`);
-        word = pieces[0];
-      }
-    }
-
-    const doc = await parser.getDocs(uri, document.getText());
-
-    if (doc) {
-      if (isFree) {
-        Linter.getErrors(
-          {
-            uri,
-            content: document.getText()
-          },
-          {
-            CollectReferences: true
-          },
-          doc
-        );
+      if (word) {
+        if (word.endsWith(`;`)) {
+          const pieces = word.split(`;`);
+          word = pieces[0];
+        }
       }
 
-      const def = doc.findDefinition(currentPos.line, word);
-      
-      if (def) {
-        const currentSelectedRef = def?.references.find(r => document.positionAt(r.offset.position).line === currentPos.line);
+      const doc = await parser.getDocs(uri, document.getText());
 
-        if (currentSelectedRef) {
-          return Range.create(
-            document.positionAt(currentSelectedRef.offset.position),
-            document.positionAt(currentSelectedRef.offset.end)
-          )
+      if (doc) {
+        if (isFree) {
+          Linter.getErrors(
+            {
+              uri,
+              content: document.getText()
+            },
+            {
+              CollectReferences: true
+            },
+            doc
+          );
+        }
+
+        const def = doc.findDefinition(currentPos.line, word);
+
+        if (def) {
+          const currentSelectedRef = def?.references.find(r => document.positionAt(r.offset.position).line === currentPos.line);
+
+          if (currentSelectedRef) {
+            return Range.create(
+              document.positionAt(currentSelectedRef.offset.position),
+              document.positionAt(currentSelectedRef.offset.end)
+            )
+          } else {
+            return selected.range;
+          }
         }
       }
     }
