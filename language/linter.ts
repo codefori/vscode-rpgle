68bac2d90554c72e2668f33209ebc8f398b40cf8
worksofliam <mrliamallan@live.co.uk>
2024-09-18 12:18:48 -0400

    Only search procedures in the current document
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- language/linter.ts
+++ language/linter.ts
@@ -160,6 +160,7 @@ export default class Linter {
         // Linter checking
         if (ruleCount > 0 && ![skipRules.single, skipRules.singleRules].includes(currentRule)) {
 
+          // const currentProcedure = globalScope.procedures.find(proc => proc.position.path === data.uri && lineNumber >= proc.range.start && lineNumber <= proc.range.end);
           const currentProcedure = globalScope.procedures.find(proc => lineNumber >= proc.range.start && lineNumber <= proc.range.end);
           const currentScope = globalScope.merge(inProcedure && currentProcedure ? currentProcedure.scope : undefined);
 
