9cd63e9fda02edf0c479993097c595d7c258f895
worksofliam <mrliamallan@live.co.uk>
2025-05-06 14:21:46 -0400

    Assurance test for #209
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- tests/suite/linter.test.ts
+++ tests/suite/linter.test.ts
@@ -3725,3 +3725,52 @@ test("DS template references", async () => {
   }
 
 });
+
+test('embedded sql with variable references (#209)', async () => {
+  const lines = [
+    `**free`,
+    `Ctl-Opt DftActGrp(*no)ActGrp(*StgMdl)`,
+    `StgMdl(*SNGLVL)`,
+    `Thread(*Concurrent)`,
+    `Option(*NoUnRef :*SrcStmt :*NoDebugIo)`,
+    `DatFmt(*Iso) TimFmt(*Iso)`,
+    `Debug(*Constants)`,
+    `Expropts(*AlwBlankNum)`,
+    `AlwNull(*UsrCtl)`,
+    `DftName(TESTFORM)`,
+    `Text('Test for format document');`,
+    `// ____________________________________________________________________________`,
+    ``,
+    `Dcl-s Ostpc Char(10);`,
+    ``,
+    `Exec Sql`,
+    `  Set Option Datfmt = *Iso, Timfmt = *Iso, Closqlcsr = *EndMod, Commit = *none;`,
+    ``,
+    `Ostpc = 'TEST';`,
+    ``,
+    `Exec Sql`,
+    `  update testupdate`,
+    `  set ostpc = :OStPc`,
+    `  where ococl = '20470' and otito = 'FCT';`,
+    ``,
+    `Return;`,
+  ].join(`\n`);
+
+  const cache = await parser.getDocs(uri, lines, { ignoreCache: true, withIncludes: true, collectReferences: true });
+  const { errors } = Linter.getErrors({ uri, content: lines }, {
+    IncorrectVariableCase: true,
+    SQLHostVarCheck: true
+  }, cache);
+
+  expect(errors.length).toBe(2);
+
+  const hostError = errors[0];
+  expect(hostError.type).toBe(`SQLHostVarCheck`);
+  expect(lines.substring(hostError.offset.start, hostError.offset.end)).toContain(`ostpc`);
+  expect(hostError.newValue).toBe(`:ostpc`);
+
+  const caseError = errors[1];
+  expect(caseError.type).toBe(`IncorrectVariableCase`);
+  expect(lines.substring(caseError.offset.start, caseError.offset.end)).toContain(`OStPc`);
+  expect(caseError.newValue).toBe(`Ostpc`);
+})
