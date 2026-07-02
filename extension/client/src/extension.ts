d90697581803e1e361d843fc5b6665927232e337
Bob Cozzi <cozzi@rpgiv.com>
2026-07-02 12:09:32 -0500

    feat(bracket): add Jump to Matching Block command

--- extension/client/src/extension.ts
+++ extension/client/src/extension.ts
@@ -8,7 +8,7 @@ import { workspace, ExtensionContext } from 'vscode';
 
 import * as Linter from "./linter";
 import * as columnAssist from "./language/columnAssist";
-import { registerBracketMatcher } from "./language/bracketMatcher";
+import { registerBracketMatcher, registerJumpToMatchingBlock } from "./language/bracketMatcher";
 import { registerCommentStatementCommand, registerUncommentStatementCommand, registerToggleCommentCommand } from './commentStmt';
 
 
@@ -101,12 +101,13 @@ export function activate(context: ExtensionContext) {
 	Linter.initialise(context);
 	columnAssist.registerColumnAssist(context);
 	registerBracketMatcher(context);
+	registerJumpToMatchingBlock(context);
 	registerCommentStatementCommand(context);
 	registerUncommentStatementCommand(context);
 	registerToggleCommentCommand(context);
-	
+
 	registerCommands(context, client);
-	
+
 	context.subscriptions.push(getServerSymbolProvider());
 	context.subscriptions.push(getServerImplementationProvider());
 	context.subscriptions.push(setLanguageSettings());
